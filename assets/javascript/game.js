3f3507fa2214816b78997fea205ac70e46787500
Adam Miller <miad0801@gmail.com>
2019-07-23 18:19:55 -0500

    Updated in progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,6 @@ $(".btn-lg").on("click", function() {
 	var emeraldNum = 0;
 
 	var targetNumber = Math.floor(Math.random() * 120) + 19;
-	console.log("The number to match is " + targetNumber);
 	$("#targetNumber").html(targetNumber);
 	$("#winCount").html(wins);
 	$("#lossCount").html(losses);
@@ -18,22 +17,18 @@ $(".btn-lg").on("click", function() {
 	$("#yourScore").html(counter);
 
 	rubyNum = Math.floor(Math.random() * 12) + 1;
-	console.log("The ruby random number is " + rubyNum);
 	var ruby = $("#ruby");
 	ruby.attr("data-crystalvalue", rubyNum);
 
 	diamondNum = Math.floor(Math.random() * 12) + 1;
-	console.log("The diamond random number is " + diamondNum);
 	var diamond = $("#diamond");
 	diamond.attr("data-crystalvalue", diamondNum);
 
 	sapphireNum = Math.floor(Math.random() * 12) + 1;
-	console.log("The sapphire random number is " + sapphireNum);
 	var sapphire = $("#sapphire");
 	sapphire.attr("data-crystalvalue", sapphireNum);
 
 	emeraldNum = Math.floor(Math.random() * 12) + 1;
-	console.log("The emerald random number is " + emeraldNum);
 	var emerald = $("#emerald");
 	emerald.attr("data-crystalvalue", emeraldNum);
 
@@ -50,7 +45,7 @@ $(".btn-lg").on("click", function() {
       		$("#winCount").html(wins);
     	}
     	else if (counter > targetNumber) {
-      		$("#alert").append("<div class='alert alert-danger' role='alert'>You lost, select the Start Game button to start the next game. Better Luck next time!</div>");
+      		$("#alert").append("<div class='alert alert-danger' role='alert'>You lost, select the Start Game button to start the next game. Better Luck next time.</div>");
       		losses++;
       		counter = 0;
       		$("#lossCount").html(losses);
